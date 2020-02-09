46ce6cd3f021473d0a119f80bdcf402b1403794e
Jongha Lim <keshasha@gmail.com>
2020-02-09 14:15:15 +0100

    register/login implement

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 // application/x-www-form-urlencoded 분석하여 가져옴
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json 분석하여 가져옴
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const config = require("./config/key");
 
@@ -34,6 +36,38 @@ app.post("/register", (req, res) => {
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // 1. 요청된 이메일이 DB에 있는지 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "Email이 존재하지 않습니다."
+      });
+    } else {
+      // 2. 비밀번호 확인
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch)
+          return res.json({
+            loginSuccess: false,
+            message: "비밀번호가 틀렸습니다."
+          });
+        else {
+          // 3. User Token 생성
+          user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            // 4. Save the Token
+            res
+              .cookie("x_auth", user.token)
+              .status(200)
+              .json({ loginSuccess: true, userID: user._id });
+          });
+        }
+      });
+    }
   });
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- models/User.js
+++ models/User.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const jwt = require("jsonwebtoken");
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -12,7 +17,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 30
+    maxlength: 80
   },
   // 사용자 유형
   role: {
@@ -28,6 +33,45 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 });
+
+// save 이전 preprocess
+userSchema.pre("save", function(next) {
+  var user = this;
+  // password 가 변환 될때만
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) return next(err);
+      else {
+        bcrypt.hash(user.password, salt, function(err, hashed) {
+          if (err) return next(err);
+          else {
+            user.password = hashed;
+            next();
+          }
+        });
+      }
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if (err) return callback(err);
+    else callback(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function(callback) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  user.token = token;
+  user.save(function(err, user) {
+    if (err) return callback(err);
+    else return callback(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
